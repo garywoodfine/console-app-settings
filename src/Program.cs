88dcec8b765b814fbc3c60c3e1514db937d914b5
Gary Woodfine <gary.woodfine@threenine.co.uk>
2018-08-05 09:04:10 +0100

    Tidy up the  arguments

--- src/Program.cs
+++ src/Program.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 
+
+
+
+
 namespace FizzBuzz
 {
     class Program
     {
         static void Main(string[] args)
         {
+           
             Console.WriteLine($" Hello {args[0] } !");
         }
     }
