1914d437fd027ac87ced217b6269810388696538
Gary Woodfine <gary.woodfine@threenine.co.uk>
2018-08-14 10:02:54 +0100

    strongly typed complete

---
+++ tests/ConfigurationTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace FizzBuzz.Tests
+{
+    public class ConfigurationTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
