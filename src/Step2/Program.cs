4e6a9674190c689c3c95d431c4a1ae9f621dae2c
Gary Woodfine <gary.woodfine@threenine.co.uk>
2020-07-02 16:35:01 +0100

    Step2 updated

--- src/Step2/Program.cs
+++ src/Step2/Program.cs
@@ -1,11 +1,23 @@
 ﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
 namespace FizzBuzz
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"Hello {args[0]}!");
+             var builder = new ConfigurationBuilder()
+                           .SetBasePath(Directory.GetCurrentDirectory())
+                           .AddJsonFile("appsettings.json");
+            
+                       IConfiguration config = new ConfigurationBuilder()
+                           .AddJsonFile("appsettings.json", true, true)
+                           .Build();
+           
+                       
+                       Console.WriteLine($" Hello { config["name"] } !");
         }
     }
 }
